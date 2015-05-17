4e25a2fb4a44c7d5b0baf94b330600ceba15e4c5
zdmilot <zdmilot@gmail.com>
2015-05-17 01:02:10 -0400

    Fixed Department

--- New User Creation.ps1
+++ New User Creation.ps1
@@ -83,14 +83,54 @@ $user = $First[0] + $Last
 
 
 
-$Dept = Read-Host "Enter Department"
+:serviceloop while(1) {
+    $Dept = Read-Host "Enter Department"
+    switch ($Dept)
+        {
+            "Field" {$Dept = "Field Technician"}
+            "Field Technician" {$Dept = "Field Technician"}
+            "Field Tech" {$Dept = "Field Technician"}
+            "Field Tech." {$Dept = "Field Technician"}
+            "FT" {$Dept = "Field Technician"}
+            "Ops" {$Dept = "Operations"}
+            "Operations" {$Dept = "Operations"}
+            "Transportation and Disposal" {$Dept = "Transportation and Disposal"}
+            "Transportation & Disposal" {$Dept = "Transportation and Disposal"}
+            "Transportation" {$Dept = "Transportation and Disposal"}
+            "Disposal" {$Dept = "Transportation and Disposal"}
+            "T & D" {$Dept = "Transportation and Disposal"}
+            "T&D" {$Dept = "Transportation and Disposal"}
+            "TD" {$Dept = "Transportation and Disposal"}
+            "T and D" {$Dept = "Transportation and Disposal"}
+            "Transport" {$Dept = "Transportation and Disposal"}
+            "Project Manager" {$Dept = "Project Manager"}
+            "Project" {$Dept = "Project Manager"}
+            "PM" {$Dept = "Project Manager"}
+            "Human Resources" {$Dept = "Human Resources"}
+            "Human" {$Dept = "Human Resources"}
+            "Resources" {$Dept = "Human Resources"}
+            "Resources" {$Dept = "Human Resources"}
+            "Information Technology" {$Dept = "Information Technology"}
+            "Information Services" {$Dept = "Information Technology"}
+            "IT" {$Dept = "Information Technology"}
+            "IS" {$Dept = "Information Technology"}
 
-$Correct3 = Read-Host "If This is Correct Press Enter, If not type n then enter"
 
-if ($Correct3 -eq "no")
-        {$Last = Read-Host "Sorry try it again"}
-elseif ($Correct3 -eq "n")
-        {$Last = Read-Host "Sorry try it again"}
+            default {
+                Write-Host "Invalid response."
+                continue serviceloop
+            }
+        }
+
+    Write-Host $Dept
+
+    $Correct5 = Read-Host "If This is Correct Press Enter, If not type n then enter"
+ 
+    if ($Correct5 -ne "n" -or $Correct5 -ne "no") {
+        break
+    }
+        
+    }
 
 
 
