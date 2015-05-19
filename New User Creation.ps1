4f9d098595055bf9e6e98d2189c7359da10f9a90
zdmilot <zdmilot@gmail.com>
2015-05-19 09:06:16 -0400

    Added CSV Creation

--- New User Creation.ps1
+++ New User Creation.ps1
@@ -333,12 +333,25 @@ if ($Correct3 -eq "y" -or $Correct3 -eq "Y") {
     #Add user creation code so that it can loop back and create another user afterwards (possably ask in an if loop to ask if you want to add another user)
 
     #Now Add CSV creation/appending
-
+        New-Object -TypeName PSCustomObject -Property @{
+            "Username" = $User
+            "Last Name" = $Last
+            "First Name" = $First
+            "Department" = $Dept
+            "Job Title" = $Title
+            "Company" = $Company
+            "State" = $Local
+            "City" = $ServCity
+            "Extension" = $Ext
+            "Work Phone" = $Phone
+            "Mobile Phone" = $MobilePh
+            "Email Address" = $EmailAddress
+        } | Export-Csv -Path C:\Users\Zachary\Desktop\New_Users_Added.csv -NoTypeInformation -Append
     #Now Add New-ADUser Creation
-    #New-ADUser -Name $First$Last -AccountPassword "Start02019" -ChangePasswordAtLogon 1 -City $ServCity -Company $Company -Country "USA" -Department $Dept -DisplayName $User -Division $Dept -EmailAddress $EmailAddress -Fax $Fax -GivenName $First -HomeDirectory \\leia\users\"$User" -HomeDrive "z" -Initials $Initials -MobilePhone $MobilePh [-Office <string>] -OfficePhone $Phone -Organization $Company [-PostalCode <string>] [-ProfilePath <string>] [-SamAccountName <string>] [-ScriptPath <string>] [-Server <string>] [-State <string>] [-StreetAddress <string>] [-Surname <string>] -Title $Title [-UserPrincipalName <string>] -Confirm
+        #New-ADUser -Name $First$Last -AccountPassword "Start02019" -ChangePasswordAtLogon 1 -City $ServCity -Company $Company -Country "USA" -Department $Dept -DisplayName $User -Division $Dept -EmailAddress $EmailAddress -Fax $Fax -GivenName $First -HomeDirectory \\leia\users\"$User" -HomeDrive "z" -Initials $Initials -MobilePhone $MobilePh [-Office <string>] -OfficePhone $Phone -Organization $Company [-PostalCode <string>] [-ProfilePath <string>] [-SamAccountName <string>] [-ScriptPath <string>] [-Server <string>] [-State <string>] [-StreetAddress <string>] [-Surname <string>] -Title $Title [-UserPrincipalName <string>] -Confirm
 
     #Now Add Group Association
-    #Add-ADGroupMember [-Identity] <ADGroup> [-Members] <ADPrincipal[]> [-AuthType {<Negotiate> | <Basic>}] [-Credential <PSCredential>] [-Partition <string>] [-PassThru <switch>] [-Server <string>] [-Confirm] [-WhatIf] [<CommonParameters>]
+        #Add-ADGroupMember [-Identity] <ADGroup> [-Members] <ADPrincipal[]> [-AuthType {<Negotiate> | <Basic>}] [-Credential <PSCredential>] [-Partition <string>] [-PassThru <switch>] [-Server <string>] [-Confirm] [-WhatIf] [<CommonParameters>]
 
     Write-Host "New User Added"
 
